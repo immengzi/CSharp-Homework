33464e4c86e12b860d1acab9d6b30685f0812390
immengzi <13017606416@163.com>
2023-03-11 21:59:23 +0800

    首次提交

---
+++ 计算器控制台程序/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入第一个数字：");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("请输入第二个数字：");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("请输入运算符（+、-、*、/）：");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            double result = 0;
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("错误：除数不能为0！");
+                        return;
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("错误：无效的运算符！");
+                    return;
+            }
+
+            Console.WriteLine("计算结果为：" + result);
+        }
+    }
+}
---
+++ 计算器窗体应用/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class 简易计算器 : Form
+    {
+        public 简易计算器()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            object op = comboBox1.SelectedItem;
+            double num1 = double.Parse(textBox1.Text);
+            double num2 = double.Parse(textBox2.Text);
+            switch (op.ToString())
+            {
+                case "+":
+                    textBox3.Text = (num1 + num2).ToString(); break;
+                case "-":
+                    textBox3.Text = (num1 - num2).ToString(); break;
+                case "*":
+                    textBox3.Text = (num1 * num2).ToString(); break;
+                case "/":
+                    textBox3.Text = (num1 / num2).ToString(); break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            comboBox1.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
