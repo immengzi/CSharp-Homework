1d66b63ec2659bb0e38011510bf526ec8fbad758
immengzi <13017606416@163.com>
2023-03-26 18:23:50 +0800

    添加第三次作业&重命名作业编号为数字便于排序

---
+++ 第3次作业/1_LinkedList/LinkedList/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace GenericListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+
+            // 从控制台输入元素并添加到链表中
+            Console.WriteLine("请输入整数元素，每两个元素之间有一个空格，直到换行/回车为止：");
+            string input = Console.ReadLine();
+            string[] inputArr = input.Split(' ');
+            foreach (string str in inputArr)
+            {
+                if (str != "")
+                {
+                    int num = int.Parse(str);
+                    list.Add(num);
+                }
+            }
+
+            // 使用ForEach方法打印链表元素
+            Console.WriteLine("链表元素：");
+            list.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+
+            // 使用ForEach方法求最大值
+            int max = 0;
+            list.ForEach(x => { if (x > max) max = x; });
+            Console.WriteLine("最大值：" + max);
+
+            // 使用ForEach方法求最小值
+            int min = int.MaxValue;
+            list.ForEach(x => { if (x < min) min = x; });
+            Console.WriteLine("最小值：" + min);
+
+            // 使用ForEach方法求和
+            int sum = 0;
+            list.ForEach(x => sum += x);
+            Console.WriteLine("总和：" + sum);
+        }
+    }
+
+    class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                action(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+
+    class Node<T>
+    {
+        private T data;
+        private Node<T> next;
+
+        public Node(T t)
+        {
+            data = t;
+            next = null;
+        }
+
+        public T Data
+        {
+            get => data;
+            set => data = value;
+        }
+
+        public Node<T> Next
+        {
+            get => next;
+            set => next = value;
+        }
+    }
+}
---
+++ 第3次作业/2_Alarm/ConsoleApp3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ClockApp
+{
+    class Clock
+    {
+        public event EventHandler Tick;
+        public event EventHandler Alarm;
+
+        public void Start(DateTime alarmTime)
+        {
+            while (true)
+            {
+                // 等待一秒钟
+                System.Threading.Thread.Sleep(1000);
+
+                // 触发 Tick 事件
+                OnTick();
+
+                // 在闹钟时间触发 Alarm 事件
+                if (DateTime.Now >= alarmTime)
+                {
+                    OnAlarm();
+                    alarmTime = alarmTime.AddDays(1); // 将闹钟时间设置为明天，以便继续触发 Tick 事件
+                }
+            }
+        }
+
+        protected virtual void OnTick()
+        {
+            Tick?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnAlarm()
+        {
+            Alarm?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入闹钟时间:时(0-23):分(0-59):秒(0-59）：");
+            DateTime alarmTime = DateTime.Parse(Console.ReadLine());
+
+            var clock = new Clock(); //创建一个Clock实例
+
+            clock.Tick += (sender, e) =>
+            {
+                Console.WriteLine("Tick...");
+            };
+
+            clock.Alarm += (sender, e) =>
+            {
+                Console.WriteLine("Alarm!!!");
+            };
+
+            clock.Start(alarmTime);
+
+            Console.ReadLine();
+        }
+    }
+}
