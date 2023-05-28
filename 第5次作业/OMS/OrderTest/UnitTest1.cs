2dc99959594b5e795e16af26451bfad7238bff6f
immengzi <13017606416@163.com>
2023-05-29 01:51:04 +0800

    更换测试项目架构为NUnit

--- 第5次作业/OMS/OrderTest/UnitTest1.cs
+++ 第5次作业/OMS/OrderTest/UnitTest1.cs
@@ -1,12 +1,16 @@
 namespace OrderTest
 {
-    [TestClass]
-    public class UnitTest1
+    public class Tests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [SetUp]
+        public void Setup()
         {
+        }
 
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
         }
     }
 }
